e2e2fd3234fc3bb2fef11e1ede207c52be3c6c6d
guohaomeng <3020721699@qq.com>
2022-08-05 11:04:36 +0800

    按需加载

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 import Inspect from 'vite-plugin-inspect'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
+      '@' : path.resolve(__dirname, 'src')
     },
   },
   css: {
@@ -25,5 +29,12 @@ export default defineConfig({
       defaultLocale: 'zh-cn',
     }),
     Inspect(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
+  base:"./"
 })
