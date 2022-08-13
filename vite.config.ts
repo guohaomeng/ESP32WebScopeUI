545c023c09630d3b1ab7eba9f375fd93f7701cf8
guohaomeng <3020721699@qq.com>
2022-08-13 20:50:51 +0800

    缩减生成的文件名至32字符以内

--- vite.config.ts
+++ vite.config.ts
@@ -47,9 +47,9 @@ export default defineConfig({
     },
     rollupOptions: {
       output: {
-        chunkFileNames: 'static/js/[name]-[hash].js',
-        entryFileNames: 'static/js/[name]-[hash].js',
-        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        chunkFileNames: 'static/js/[name].js',
+        entryFileNames: 'static/js/[name].js',
+        assetFileNames: 'static/[ext]/[name].[ext]',
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
